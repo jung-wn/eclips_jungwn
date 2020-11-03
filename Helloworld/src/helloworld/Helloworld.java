9229cde2a677f274757623612df0d2cbcd4a6272
wku <wku@192.168.172.190>
2020-11-03 09:40:22 +0800

    create name

--- Helloworld/src/helloworld/Helloworld.java
+++ Helloworld/src/helloworld/Helloworld.java
@@ -1,5 +1,11 @@
 package helloworld;
 
 public class Helloworld {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Jung Wn, Jo");
+		System.out.println("Computer SW engineering");
+	}
 
 }
