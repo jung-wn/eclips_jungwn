5fd98bf1cb41dbae070545ced6a93d4bcf72bfb1
jung-wn <73219225+jung-wn@users.noreply.github.com>
2020-11-10 09:32:34 +0800

    Update Pushprint.java

--- Helloworld/src/helloworld/Pushprint.java
+++ Helloworld/src/helloworld/Pushprint.java
@@ -5,6 +5,6 @@ public class Pushprint {
 	System.out.println("Sophomore");
 	System.out.println("Wankang univ.");
 	System.out.println("JeonBuk Iksan");
-	System.out.println("fighting!")
+	System.out.println("fighting!");
 	}
 }
